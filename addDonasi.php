36b65f84ff7c7d93dc8d9cf0d7eb4707e3bee3ff
odilia12 <odiliayulialina99@gmail.com>
2020-06-08 20:46:08 +0700

    Update addDonasi.php

--- addDonasi.php
+++ addDonasi.php
@@ -3,4 +3,47 @@ header("Access-Control-Allow-Origin: *");
 
 require_once('database.php');
 require_once('functions.php');
+
+$username = sanitize($_POST['username']);
+$tanggal = $_POST['bod'];
+
+$query = "INSERT INTO donasi (tanggal, email_user) 
+		VALUES ('$tanggal','$username')";
+
+$sql = $conn7->query($query);
+
+if ($sql) {
+	$sql2 = $conn->query("
+	SELECT id FROM donasi WHERE tanggal = '$tanggal' AND email_user = '$username'
+	");
+	if($sql){ 	
+		$datas = array();
+		$i = 0;
+		while ($res = $sql->fetch_assoc()) {
+			$datas['datas'][$i]['id'] = $res['id'];
+			$i++;
+		}
+		echo json_encode($datas);
+
+		$id_donasi = $datas['datas'][0]['id'];
+
+		$query3 = "INSERT INTO detail_donasi (id_donasi, id_kebutuhan, nilai) 
+			VALUES ('$id_donasi','$id_kebutuhan','$nilai')";
+
+		$sql3 = $conn7->query($query2);
+
+		if($sql3){
+			echo json_encode("success");
+		}
+		else{
+			echo "gagal menambah donasi";
+		}
+	}
+	else{
+		echo "gagal menambah donasi";
+	}
+} 
+else{
+		echo "gagal menambah donasi";
+}
 ?>
